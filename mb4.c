e07a45a6dc560f2f3ff9e62043d787e35a426d5b
mohit bharwani <padi.ashok2007@gmail.com>
2025-09-02 00:01:30 +0530

    Create mb4.c

---
+++ mb4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() 
+{
+    int n, i;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    printf("First %d natural numbers:\n", n);
+    for(i = 1; i <= n; i++) 
+{
+        printf("%d ", i);
+}
+
+    return 0;
+}
